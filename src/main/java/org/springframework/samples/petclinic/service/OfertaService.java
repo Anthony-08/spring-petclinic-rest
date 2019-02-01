ed5bd59fd994106e4bbca65410ad3048b2d5c6fc
formacion <formacion@DESKTOP-I3BSGFG.mshome.net>
2019-02-01 10:12:00 +0100

    OfertaService creada

---
+++ src/main/java/org/springframework/samples/petclinic/service/OfertaService.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.samples.petclinic.model.Oferta;
+
+public interface OfertaService {
+
+   Oferta findById(int id);
+
+   List<Oferta> findAll();
+
+   Oferta guardarOferta(Oferta oferta);
+
+   void borrarOferta(int id);
+
+}
