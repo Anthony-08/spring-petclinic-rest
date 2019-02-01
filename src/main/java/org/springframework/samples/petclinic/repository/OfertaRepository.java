bbd19b45de06da016ff8b947f81a56a64f95fc83
Julian Lopez <julianlopezjimenez@gmail.com>
2019-02-01 09:47:21 +0100

    Creacion del Repositorio

---
+++ src/main/java/org/springframework/samples/petclinic/repository/OfertaRepository.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.repository;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.samples.petclinic.model.Oferta;
+
+@Resource
+public interface OfertaRepository extends JpaRepository<Oferta, Integer> {
+
+	@Query("select o from Oferta o where o.fecha >= CURRENT_TIME")
+	List<Oferta> findOfertasVigentes();
+
+}
